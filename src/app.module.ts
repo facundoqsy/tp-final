247ef5f311cb38b7b82c603ed141d18d2e823f85
unknown <facundoaseren@gmail.com>
2025-11-10 21:26:27 -0300

    Borrado completo del contenido

--- src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VeterinariaModule } from './veterinaria/veterinaria.module';
-
-@Module({
-  imports: [VeterinariaModule],
-})
-export class AppModule {}
-//Este archivo es el módulo principal de la app, importa el módulo de la veterinaria para que NestJS lo use.
--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
-//Este archivo es un servicio simple, tiene una función getHello() que da el texto "Hello World!" y El controlador lo usa para responder.
